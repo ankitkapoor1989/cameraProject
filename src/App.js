77b2d2615cb5f01762f37eea0961b0d262bb3b72
ankitkapoor1989 <85103494+ankitkapoor1989@users.noreply.github.com>
2022-02-21 17:29:16 +0530

    Camera Project

--- src/App.js
+++ src/App.js
@@ -1,69 +1,30 @@
-import React, { useState } from "react";
-import MobileDetect from "mobile-detect";
+import React, { useState, useEffect } from "react";
+import {
+  osName
+} from 'react-device-detect'
+import Webcam from "react-webcam";
 import "./App.css";
 
 function App() {
-  const [playing, setPlaying] = useState(false);
+  const [playing, setPlaying] = useState('Pristine OS');
 
-  const HEIGHT = 500;
-  const WIDTH = 500;
+  useEffect(() => {
+    setPlaying(osName)
+  }, [])
 
-  const startVideo = () => {
-    setPlaying(true);
-    navigator.getUserMedia(
-      {
-        video: true,
-      },
-      (stream) => {
-        let video = document.getElementsByClassName("app__videoFeed")[0];
-        if (video) {
-          video.srcObject = stream;
-        }
-      },
-      (err) => console.error(err)
-    );
+  const videoConstraints = {
+    facingMode: "user"
   };
-
-  const stopVideo = () => {
-    setPlaying(false);
-    let video = document.getElementsByClassName("app__videoFeed")[0];
-    video.srcObject.getTracks()[0].stop();
-  };
-  const deviceChange = new MobileDetect(window.navigator.userAgent);
-  let deviceName = "";
-  if (deviceChange.os() === "iOS") {
-    deviceName = "Model: iPhone";
-  } else if (deviceChange.os() === "AndroidOS") {
-    deviceName = "Model: Android";
-  } else if (deviceChange.os() === "BlackBerryOS") {
-    deviceName = "Model: BlackBerry";
-  } else if (deviceChange.os() === "WindowsOS") {
-    deviceName = "Model: Window";
-  } else if (deviceChange.os() === "MacOS") {
-    deviceName = "Model: Mac";
-  } else {
-    deviceName = "Model: Mac";
-  }
 
   return (
     <div className="app">
       <div className="app__container">
-        <video
-          height={HEIGHT}
-          width={WIDTH}
-          muted
-          autoPlay
-          className="app__videoFeed"
-        ></video>
+      <Webcam 
+        height='65%'
+        videoConstraints={videoConstraints}
+      />
       </div>
-      <div className="app__input">
-        {playing ? (
-          <button onClick={stopVideo}>Stop</button>
-        ) : (
-          <button onClick={startVideo}>Start</button>
-        )}
-      </div>
-      <div className="app__deviceName">{deviceName}</div>
+      <div className="app__deviceName"> Model - {playing}</div>
     </div>
   );
 }
